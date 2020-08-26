e882641f598d95f9517b7cfb05b02d161de20b58
abdikasa <abdikasa@sheridancollege.ca>
2020-08-26 16:11:23 -0400

    Finished ex 1.7: Added a Statistics Component to hold the stats

--- src/index.js
+++ src/index.js
@@ -2,23 +2,39 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// 1.7: unicafe step2
-// Expand your application so that it shows more statistics about the gathered feedback: the total number of collected feedback, the average score (good: 1, neutral: 0, bad: -1) and the percentage of positive feedback.
+// 1.8: unicafe step3
+// Refactor your application so that displaying the statistics is extracted into its own Statistics component. The state of the application should remain in the App root component.
+
+const Statistics = (prop) => {
+  const sum = prop.g + prop.n + prop.b;
+  const avg = sum / 3;
+  const pos = () => {
+    const formula = prop.g / sum;
+    if (!isNaN(formula)) {
+      return prop.g / (prop.g + prop.b + prop.n);
+    } else {
+      return 0;
+    }
+  };
+
+  return (
+    <>
+      <h2>Statistics</h2>
+      <p>good {prop.g}</p>
+      <p>neutral {prop.n}</p>
+      <p>bad {prop.b}</p>
+      <p>all {sum}</p>
+      <p>average {avg.toFixed(2)}</p>
+      <p>positive {pos().toFixed(2)}%</p>
+    </>
+  );
+};
 
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-
-  const calcPos = () => {
-    let formula = good / (good + bad + neutral);
-    if (!isNaN(formula)) {
-      return good / (good + bad + neutral);
-    } else {
-      return 0;
-    }
-  };
 
   return (
     <div>
@@ -44,13 +60,7 @@ const App = () => {
       >
         bad
       </button>
-      <h2>Statistics</h2>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + bad + neutral}</p>
-      <p>average {((good + bad + neutral) / 3).toFixed(2)}</p>
-      <p>positive {calcPos().toFixed(2)}%</p>
+      <Statistics g={good} b={bad} n={neutral}></Statistics>
     </div>
   );
 };
