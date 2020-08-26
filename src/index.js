47f9a23d1d18f329c8a0c83292cd391f407daeda
abdikasa <abdikasa@sheridancollege.ca>
2020-08-26 15:36:16 -0400

    Starting the project, added boiler plate

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// 1.6: unicafe step1
+// Like most companies, Unicafe collects feedback from its customers. Your task is to implement a web application for collecting customer feedback. There are only three options for feedback: good, neutral, and bad.
+
+// The application must display the total number of collected feedback for each category.
+
+import ReactDOM from "react-dom";
+
 const App = () => {
-  return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  );
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return <div>code here</div>;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
