01daa54f76c0c7d3d73c66627ba141a533a684ca
abdikasa <abdikasa@sheridancollege.ca>
2020-08-26 15:48:49 -0400

    Completed the 1.6 exercise

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
@@ -7,15 +7,42 @@ import "./index.css";
 
 // The application must display the total number of collected feedback for each category.
 
-import ReactDOM from "react-dom";
-
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  return <div>code here</div>;
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button
+        onClick={() => {
+          setGood(good + 1);
+        }}
+      >
+        good
+      </button>
+      <button
+        onClick={() => {
+          setNeutral(neutral + 1);
+        }}
+      >
+        neutral
+      </button>
+      <button
+        onClick={() => {
+          setBad(bad + 1);
+        }}
+      >
+        bad
+      </button>
+      <h2>Statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
